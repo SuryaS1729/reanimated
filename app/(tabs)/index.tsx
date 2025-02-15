b0ea7f20425341b8bb28877459eda0301e4565a0
SuryaS1729 <surya.curiousfusion@gmail.com>
2025-02-15 13:23:02 +0530

    useAnimatedStyles and useAnimatedProps, with Timing and withSpring

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,25 +1,119 @@
-import { Button } from 'react-native';
-import Animated, { withSpring } from 'react-native-reanimated';
-import { useSharedValue } from 'react-native-reanimated';
-import { View } from 'react-native';
+// import { Button } from 'react-native';
+// import Animated, { withSpring } from 'react-native-reanimated';
+// import { useSharedValue } from 'react-native-reanimated';
+// import { View } from 'react-native';
+
+// export default function App() {
+
+//   const width= useSharedValue(100)
+//   const handlePress=  ()=>{
+//     width.value = withSpring(width.value+50)
+//   }
+
+//   return (
+//     <View style = {{flex:1, alignItems:'center'}}>
+//       <Animated.View
+//         style={{
+//           width: width,
+//           height: 100,
+//           borderRadius:10,
+//           backgroundColor: 'red',
+//         }}
+//       />
+//        <Button onPress={handlePress} title="Click me" />
+//     </View>
+//   );
+// }
+
+// import React from 'react';
+// import { Button, View, StyleSheet } from 'react-native';
+// import Animated, {
+//   useSharedValue,
+//   withSpring,
+//   useAnimatedStyle,
+// } from 'react-native-reanimated';
+
+// export default function App() {
+//   const translateY = useSharedValue<number>(20);
+
+//   const handlePress = () => {
+//     translateY.value += 50;
+//   };
+
+//   const animatedStyles = useAnimatedStyle(() => ({
+//     transform: [{ translateY: withSpring(translateY.value*5 ) }],
+//   }));
+
+//   return (
+//     <>
+//       <Animated.View style={[styles.box, animatedStyles]} />
+//       <View style={styles.container}>
+//         <Button onPress={handlePress} title="Click me" />
+//       </View>
+//     </>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   box: {
+//     height: 120,
+//     width: 120,
+//     backgroundColor: '#b58df1',
+//     borderRadius: 20,
+//     marginVertical: 50,
+//   },
+// });
+
+import React from 'react';
+import { Button, View, StyleSheet } from 'react-native';
+import Animated, {
+  useSharedValue,
+  useAnimatedProps,
+  withTiming,
+  withSpring
+} from 'react-native-reanimated';
+import { Svg, Circle } from 'react-native-svg';
+
+const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 
 export default function App() {
+  const l = useSharedValue<number>(20);
 
-  const width= useSharedValue(100)
-  const handlePress=  ()=>{
-    width.value = withSpring(width.value+50)
-  }
+  const handlePress = () => {
+    l.value += 10;
+  };
+
+  const animatedProps = useAnimatedProps(() => ({
+    r: withTiming(l.value),
+  }));
 
   return (
-    <View style = {{flex:1, alignItems:'center'}}>
-      <Animated.View
-        style={{
-          width: width,
-          height: 100,
-          backgroundColor: 'violet',
-        }}
-      />
-       <Button onPress={handlePress} title="Click me" />
+    <View style={styles.container}>
+      <Svg style={styles.svg}>
+        <AnimatedCircle
+          cx="50%"
+          cy="50%"
+          fill="#b58df1"
+          animatedProps={animatedProps}
+        />
+      </Svg>
+      <Button onPress={handlePress} title="Click me" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  svg: {
+    height: 250,
+    width: '100%',
+  },
+});
