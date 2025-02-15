9ddaa98e041550a705f2d53527dbac2e2c9a8035
SuryaS1729 <surya.curiousfusion@gmail.com>
2025-02-15 16:46:36 +0530

    example

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -69,51 +69,101 @@
 //   },
 // });
 
-import React from 'react';
-import { Button, View, StyleSheet } from 'react-native';
-import Animated, {
-  useSharedValue,
-  useAnimatedProps,
-  withTiming,
-  withSpring
-} from 'react-native-reanimated';
-import { Svg, Circle } from 'react-native-svg';
+// import React from 'react';
+// import { Button, View, StyleSheet } from 'react-native';
+// import Animated, {
+//   useSharedValue,
+//   useAnimatedProps,
+//   withTiming,
+//   withSpring
+// } from 'react-native-reanimated';
+// import { Svg, Circle } from 'react-native-svg';
 
-const AnimatedCircle = Animated.createAnimatedComponent(Circle);
+// const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 
-export default function App() {
-  const l = useSharedValue<number>(20);
+// export default function App() {
+//   const l = useSharedValue<number>(20);
 
-  const handlePress = () => {
-    l.value += 10;
-  };
+//   const handlePress = () => {
+//     l.value += 10;
+//   };
 
-  const animatedProps = useAnimatedProps(() => ({
-    r: withTiming(l.value),
-  }));
+//   const animatedProps = useAnimatedProps(() => ({
+//     r: withTiming(l.value),
+//   }));
 
-  return (
-    <View style={styles.container}>
-      <Svg style={styles.svg}>
-        <AnimatedCircle
-          cx="50%"
-          cy="50%"
-          fill="#b58df1"
-          animatedProps={animatedProps}
-        />
-      </Svg>
-      <Button onPress={handlePress} title="Click me" />
-    </View>
-  );
-}
+//   return (
+//     <View style={styles.container}>
+//       <Svg style={styles.svg}>
+//         <AnimatedCircle
+//           cx="50%"
+//           cy="50%"
+//           fill="#b58df1"
+//           animatedProps={animatedProps}
+//         />
+//       </Svg>
+//       <Button onPress={handlePress} title="Click me" />
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  svg: {
-    height: 250,
-    width: '100%',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//   },
+//   svg: {
+//     height: 250,
+//     width: '100%',
+//   },
+// });
+
+
+// import Animated, {
+//   useSharedValue,
+//   withTiming,
+//   useAnimatedStyle,
+//   Easing,
+// } from 'react-native-reanimated';
+// import { View, Button, StyleSheet } from 'react-native';
+
+// export default function AnimatedStyleUpdateExample() {
+//   const randomWidth = useSharedValue(10);
+
+//   const config = {
+//     duration: 500,
+//     easing: Easing.bezier(0.5, 0.01, 0, 1),
+//   };
+
+//   const style = useAnimatedStyle(() => {
+//     return {
+//       width: withTiming(randomWidth.value, config),
+//     };
+//   });
+
+//   return (
+//     <View style={styles.container}>
+//       <Animated.View style={[styles.box, style]} />
+//       <Button
+//         title="toggle"
+//         onPress={() => {
+//           randomWidth.value = Math.random() * 350;
+//         }}
+//       />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   box: {
+//     width: 100,
+//     height: 80,
+//     backgroundColor: 'white',
+//     margin: 30,
+//   },
+// });
